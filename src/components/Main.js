9ffbb7dc10f530e1b5017468bcf30d2e842b34f0
heeja <holeonline@naver.com>
2019-11-21 16:10:44 +0900

    Main QuizList - 1st Worked

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
-// import { QuizInfo } from "./quizInfo";
+import { baseURL } from "../common/config";
+import Authaxios from "../common/authAxios";
 
 export default function Main(quizs, getNextPage) {
   const styles = {
@@ -24,28 +25,39 @@ export default function Main(quizs, getNextPage) {
     }
   };
 
+  // const res = async () => {
+  //   const list = await Authaxios().get(`${baseURL}/admin`);
+  //   // console.log(list.data.allQuizzes[0]);
+  //   const mapList = list.map();
+  //   console.log(mapList);
+  // };
+  // res();
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-lg-8 col-md-10 mx-auto">
           <div className="post-preview">
-            {/* <Link className="nav-link" to={`/:${useParams.params.id}`}> */}
-            <Link
-              className="nav-link"
-              to={`/quiz/${quizs._id}`}
-              style={styles.container}
-            >
-              <div style={styles.item1}>
-                <h2>1</h2>
-              </div>
-              <div style={styles.item2}>
-                <h2 className="post-title">Quiz - Title</h2>
-                <h3 className="post-subtitle">Quiz -</h3>
-                <p className="post-meta">
-                  Quiz ID / Quiz Category / Quiz Date / Latitude, Longitude /
-                </p>
-              </div>
-            </Link>
+            {quizs &&
+              quizs.map(quiz => (
+                <Link
+                  className="nav-link"
+                  to={`/quiz/${quizs._id}`}
+                  style={styles.container}
+                >
+                  <div style={styles.item1}>
+                    <h2>1</h2>
+                  </div>
+                  <div style={styles.item2}>
+                    <h2 className="post-title">Quiz - Title</h2>
+                    <h3 className="post-subtitle">Quiz -</h3>
+                    <p className="post-meta">
+                      Quiz ID / Quiz Category / Quiz Date / Latitude, Longitude
+                      /
+                    </p>
+                  </div>
+                </Link>
+              ))}
             <hr />
             <Link
               className="nav-link"
